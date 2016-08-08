fe71fc3b77a90e46d7280762f9d6c9d9ed8d80e4
shubhi22 <shubhi.srvstv@gmail.com>
2016-08-08 21:12:12 +0530

    Create Staircase.java

---
+++ algorithms/Warmup/Staircase.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            for(int j=i+1;j<=n;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++)
+                {
+                 System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
